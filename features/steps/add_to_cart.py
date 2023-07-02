8f35de9f92fe32347c010bf37977efe20fc1deb2
frances gibson <francesgibson@francess-Air.attlocal.net>
2023-07-02 13:49:02 -0600

    add to cart hw

---
+++ features/steps/add_to_cart.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+from behave import when, given, then
+from time import sleep
+
+
+RESULT_TEXT = (By.XPATH, "//span[@class='a-color-state a-text-bold']")
+PRODUCT_PRICE = (By.XPATH, "//div[@data-component-type='s-search-result']//a[.//span[@class='a-price']]")
+PRODUCT_NAME = (By.ID, 'productTitle')
+COLOR_OPTIONS = (By.CSS_SELECTOR, "#variation_color_name li")
+CURRENT_COLOR = (By.CSS_SELECTOR, "#variation_color_name .selection")
+ADD_TO_CART_BTN = (By. ID, 'add-to-cart-button')
+
+
+@given('Open Amazon page')
+def open_google(context):
+    context.driver.get('https://www.amazon.com/')
+
+
+
+@when('Click on Add to cart button')
+def click_add_to_cart(context):
+    context.driver.find_element(*ADD_TO_CART_BTN).click()
+    sleep(2)
+
+@when('Click on the first product')
+def click_first_product(context):
+    context.driver.find_element(*PRODUCT_PRICE).click()
+    sleep(2)
+
+@when('Open Cart page')
+def open_cart_page(context):
+    context.driver.get('https://www.amazon.com/gp/cart/view.html?ref_=sw_gtc')
+
+
+@then('Verify cart has {expected_count" item(s)')
+def verify_cart_count(context, expected_count):
+    actual_text = context.driver.find_element(*CART).text
+    assert expected_count == actual_text, f'Expected {expected_count}'
